7dadaf6932f4e4f940ff4ccdeed2e586ead53cf7
busratok <busratokuc@gmail.com>
2023-07-18 20:32:20 +0100

    axios is installed and variable places are changed for scope concern.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,11 @@ import { Container } from "react-bootstrap";
 import SearchBar from "../components/SearchBar";
 import { useState } from "react";
 
-const API_KEY = process.env.REACT_APP_TMDB_KEY;
-const MULTI_SEARCH_API = `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&query=${query}`;
-
 const Home = () => {
   const [query, setQuery] = useState("");
   const [multiData, setMultiData] = useState([]);
+  const API_KEY = process.env.REACT_APP_TMDB_KEY;
+  const MULTI_SEARCH_API = `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&query=${query}`;
 
   return (
     <Container className="text-center mt-4">
