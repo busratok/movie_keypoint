b241627c4e29e8655b37f41d24f683191357f105
busratok <busratokuc@gmail.com>
2023-07-18 17:12:23 +0100

    user-setUser state is created and pass to Login and Home components. Handlesubmit function and onchage events is filled

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,30 +1,39 @@
-import React from "react";
+import React, { useRef } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Container } from "react-bootstrap";
 
-const Login = () => {
+const Login = ({ user, setUser }) => {
+  const formRef = useRef(null);
   const handleSubmit = (e) => {
     e.preventDefault();
+    sessionStorage.setItem("user", JSON.stringify(user));
+    formRef.current.reset();
   };
   return (
     <Container className="d-flex flex-column justify-content-center align-items-center min-vh-100">
       <h1 className="mb-5">Log In</h1>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+      <Form onSubmit={handleSubmit} ref={formRef}>
+        <Form.Group className="mb-3" controlId="email">
           <Form.Label>Email address</Form.Label>
           <Form.Control
             type="email"
             placeholder="Enter your email"
+            onChange={(e) =>
+              setUser((user) => ({ ...user, email: e.target.value }))
+            }
             required
             autoFocus
           />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Group className="mb-3" controlId="password">
           <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
             placeholder="Enter your password"
+            onChange={(e) =>
+              setUser((user) => ({ ...user, password: e.target.value }))
+            }
             required
             minLength={6}
           />
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,14 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import PrivateRouter from "./PrivateRouter";
 import Home from "../pages/Home";
+import { useState } from "react";
 
 const AppRouter = () => {
+  const [user, setUser] = useState(
+    JSON.parse(sessionStorage.getItem("user")) || false
+  );
   return (
     <Router>
       <Routes>
-        <Route path="" element={<Login />} />
+        <Route path="" element={<Login user={user} setUser={setUser} />} />
         <Route path="home" element={<PrivateRouter />}>
-          <Route index element={<Home />} />
+          <Route index element={<Home user={user} />} />
         </Route>
       </Routes>
     </Router>
