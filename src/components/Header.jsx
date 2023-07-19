082239a2f4339bbad283ede40b5808a61abe2cc7
busratok <busratokuc@gmail.com>
2023-07-19 14:09:20 +0100

    Nav.Bran link is fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,17 @@ import { useContext } from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import { Context } from "../context/Context";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   const { user } = useContext(Context);
   return (
     <Navbar className="bg-body-tertiary p-3" bg="dark" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="#home" className="fs-1">
-          Movie App
+        <Navbar.Brand href="" className="fs-1">
+          <NavLink to="/home" className="text-decoration-none text-light">
+            Movie App
+          </NavLink>
         </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
